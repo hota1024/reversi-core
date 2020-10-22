79aa325fde6cc372d40f8c6b914e2d39e6ffa67a
hota1024 <hotalog@hota1024.com>
2020-10-22 17:13:40 +0900

    🏷️ Add Grid interface

---
+++ src/interfaces/Grid.ts
@@ -0,0 +1,52 @@
+import { Point } from '@/types'
+
+/**
+ * Grid#each's callback function type.
+ */
+export type GridEachCallbackFn<T> = (value: T, point: Point) => void
+
+/**
+ * Grid#map's callback function type.
+ */
+export type GridMapCallbackFn<T, U> = (value: T, point: Point) => void
+
+/**
+ * Grid interface.
+ */
+export interface IGrid<T> {
+  /**
+   * fill by given value.
+   *
+   * @param value value to fill.
+   */
+  fill(value: T): this
+
+  /**
+   * set given value to given point.
+   *
+   * @param point point to set.
+   * @param value valeu to set.
+   */
+  set(point: Point, value: T): this
+
+  /**
+   * returns value at given point.
+   *
+   * @param point point to get.
+   */
+  get(point: Point): T
+
+  /**
+   * call given callback for each values.
+   *
+   * @param callback callback function.
+   */
+  each(callback: GridEachCallbackFn<T>): void
+
+  /**
+   * returns new grid object by return value of given callback.
+   *
+   * @param callback callback function.
+   */
+  map<U>(callback: GridMapCallbackFn<T, U>): IGrid<U>
+}
